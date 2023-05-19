ad0dd63a8a78c04b487bb94adf271368d74e1b40
ahmad-masud <ahmadmasud25@hotmail.com>
2023-05-18 22:53:32 -0700

    added footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import RateRover from './Components/RateRover/RateRover.js';
-import Header from './Components/Header/Header.js'
+import Header from './Components/Header/Header.js';
+import Footer from './Components/Footer/Footer.js';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <RateRover/>
+      <Footer/>
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,11 +8,6 @@ function Header() {
                     <img src={process.env.PUBLIC_URL + '/Logo.png'} alt='Logo' className='header-title-logo'></img>
                     <span className='header-title-text'>RATEROVER</span>
                 </div>
-                <div className='header-links'>
-                    <a href='https://github.com/ahmad-masud' className='header-link' target='_blank' rel="noreferrer"><i className='fa-brands fa-github'></i></a>
-                    <a href='https://www.linkedin.com/in/ahmadmasud/' className='header-link' target='_blank' rel="noreferrer"><i className='fa-brands fa-linkedin'></i></a>
-                    <a href='https://ahmadmasud.xyz' className='header-link' target='_blank' rel="noreferrer"><i className="fa-solid fa-globe"></i></a>
-                </div>
             </div>
         </div>
     )
